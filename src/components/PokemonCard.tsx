29c0565af28643c46341a2a5e97a6f46da2dc3ff
Vinicius de Avila Moreira <vinicius741@gmail.com>
2024-05-21 14:21:03 -0300

    mobile and code improvements

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+interface PokemonCardProps {
+    card: {
+        id: string;
+        name: string;
+        images: {
+            small: string;
+        };
+        types: string[];
+    };
+    key: string;
+}
+
+const PokemonCard: React.FC<PokemonCardProps> = ({ card, key }) => {
+    return (
+        <div className="card" key={key}>
+            <Link to={`/details/${card.id}`}>
+                <div className="image-container">
+                    <img src={card.images.small} alt={card.name} />
+                </div>
+                <h3>{card.name}</h3>
+                <p>ID: {card.id}</p>
+                <p>Tipo: {card.types.join(", ")}</p>
+            </Link>
+        </div>
+    );
+};
+
+export default PokemonCard;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,47 @@
-// src/pages/HomePage.tsx
 import React from "react";
 import { usePokemonCards } from "../context/PokemonCardsContext";
-import { Link } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 import Slider from "react-slick";
 import "../styles/homePage.scss";
+import PokemonCard from "../components/PokemonCard";
 
 const HomePage: React.FC = () => {
     const { pokemonCards } = usePokemonCards();
-    const isMobile = useMediaQuery({ maxWidth: 767 });
+    const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
 
     const settings = {
-        dots: true,
-        infinite: true,
+        dots: false,
+        infinite: false,
         speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
     };
 
     return (
@@ -23,27 +49,13 @@ const HomePage: React.FC = () => {
             {isMobile ? (
                 <Slider {...settings}>
                     {pokemonCards.map((card) => (
-                        <div key={card.id} className="card">
-                            <Link to={`/details/${card.id}`}>
-                                <img src={card.images.small} alt={card.name} />
-                                <h3>{card.name}</h3>
-                                <p>ID: {card.id}</p>
-                                <p>Type: {card.types.join(", ")}</p>
-                            </Link>
-                        </div>
+                        <PokemonCard key={card.id} card={card} />
                     ))}
                 </Slider>
             ) : (
                 <div className="grid">
                     {pokemonCards.map((card) => (
-                        <div key={card.id} className="card">
-                            <Link to={`/details/${card.id}`}>
-                                <img src={card.images.small} alt={card.name} />
-                                <h3>{card.name}</h3>
-                                <p>ID: {card.id}</p>
-                                <p>Type: {card.types.join(", ")}</p>
-                            </Link>
-                        </div>
+                        <PokemonCard key={card.id} card={card} />
                     ))}
                 </div>
             )}
